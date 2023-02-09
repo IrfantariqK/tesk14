385bdc78b5c89c292e77731dab8f67d6b088eeb4
IrfantariqK <irfan_tariqk@hotmail.com>
2023-02-09 15:20:45 +0500

    tesk14

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './counterSlice'
+import { increment, incrementByAmount } from './counterSlice'
 
 
 export function Counter() {
@@ -13,13 +13,16 @@ export function Counter() {
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
+          
         >
           Increment
         </button>
         <span>{count}</span>
         <button
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
+          onClick={() => dispatch(incrementByAmount(-5))}
+          disabled={(count - 5) < 0 ? true : false}
+          
         >
           Decrement
         </button>
